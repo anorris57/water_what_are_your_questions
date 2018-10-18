c08f0ce515402f6559c1f7a55dcc0502a4626431
Angela Norris <cocoanorris@yahoo.com>
2018-10-18 16:15:14 -0400

    created profile form

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -14,9 +14,25 @@ class CreateProfile extends Component {
       bio: '',
       errors: {}
     };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-    render() {
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+
+  onSubmit(e) {
+    e.preventDefault();
+    
+    console.log('submit');
+  }
+
+
+    render() { 
+      const { errors} = this.state;
       return (
         <div className="create-profile">
           <div className="container">
@@ -24,9 +40,35 @@ class CreateProfile extends Component {
               <div className="col-md-8 m-auto">
                 <h1 className="display-4 text-center">Create Your Profile</h1>
                 <p className="lead text-center">
-                  Let's get some information to make your profile stand out
+                  Create a handle to use in asking questions or leaving comments.
                 </p>
                 <small className="d-block pb-3">* = required fields</small>
+                <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="* Profile Handle"
+                  name="handle"
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info="A unique handle for your profile URL.  Use Your full name or a nickname."
+                /> 
+                <TextFieldGroup
+                  placeholder="Favorite Chemistry topic"
+                  name="Favorite"
+                  value={this.state.subject}
+                  onChange={this.onChange}
+                  error={errors.subject}
+                  info="Put your favorite Chemistry topic!"
+                />
+                <TextAreaFieldGroup
+                  placeholder="Short Bio"
+                  name="bio"
+                  value={this.state.bio}
+                  onChange={this.onChange}
+                  error={errors.bio}
+                  info=" Share what career field you plan to pursue."
+                />
+                </form>
               </div>
             </div>
           </div>
