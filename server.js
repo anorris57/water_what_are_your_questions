f4232c0fb0e1de7a94bc0b5981e5c4482719677c
Angela Norris <cocoanorris@yahoo.com>
2018-10-20 09:41:19 -0400

    attempting to fix port error again

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ if (process.env.NODE_ENV === 'production') {
 
 
 
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
